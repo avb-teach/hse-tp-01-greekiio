69cafa17939c0f7c0751af1fc0a78e023bab40bd
greekiio <arina.ionova867@gmail.com>
2025-04-30 21:07:13 +0300

    upload

---
+++ collect_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# unix.stackexchange.com/questions/103078/cp-backup-numbered-for-folders
+find "$1" -type f -exec cp --backup=numbered -t "$2" {} +
+
+for file in "$2"/*.~*~; do
+    # www.linux.org.ru/forum/general/2639961
+    curExt="${file%.*}"
+    ext="${curExt##*.}"
+
+    addedBackup="${file##*.}"
+    cur="${file%%.*}"
+    result="${cur}${addedBackup}"
+
+    # stackoverflow.com/questions/4181703/how-to-concatenate-string-variables-in-bash
+    if [ "$cur" != "$ext" ]; then
+        result="$result.${ext}"
+    fi
+
+    # www.linux.org.ru/forum/general/2003823
+    clearedResult="${result//\~/}"
+
+    cp -- "$file" "$clearedResult"
+    rm -- "$file"
+
+done
