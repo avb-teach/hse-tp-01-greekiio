ccf4264dda3a7155cf33e4b43940cdb46056c886
greekiio <arina.ionova867@gmail.com>
2025-04-30 23:51:39 +0300

    update

--- collect_files.sh
+++ collect_files.sh
@@ -1,33 +1,19 @@
-# unix.stackexchange.com/questions/32409/set-and-shopt-why-two
-shopt -s nullglob
+#!/bin/bash
 
-a1="$1"
-a2="$2"
-in="${a1:1}"
-out="${a2:1}"
+in="$1"
+out="$2"
 
-mkdir -p "$out"
+cnt=0
+find "$in" -type f | while read f; do
+    basename=$(basename "$f")
+    ext="${basename##*.}"
+    name="${basename%.*}"
 
-# unix.stackexchange.com/questions/103078/cp-backup-numbered-for-folders
-find "$in" -type f -exec cp --backup=numbered {} "$out" \;
+    result="${name}${cnt}"
+    ((cnt++))
+    if [[ "$name" != "$ext" ]]; then 
+        result="$result.${ext}"
+    fi
 
-# for file in "$out"/*.~*~; do
-#     # www.linux.org.ru/forum/general/2639961
-#     curExt="${file%.*}"
-#     ext="${curExt##*.}"
-
-#     addedBackup="${file##*.}"
-#     cur="${file%%.*}"
-#     result="${cur}${addedBackup}"
-
-#     # stackoverflow.com/questions/4181703/how-to-concatenate-string-variables-in-bash
-#     if [ "$cur" != "$ext" ]; then
-#         result="$result.${ext}"
-#     fi
-
-#     # www.linux.org.ru/forum/general/2003823
-#     clearedResult="${result//\~/}"
-
-#     mv -- "$file" "$clearedResult"
-
-# done
+    cp "$f" "$out/$result"
+done
